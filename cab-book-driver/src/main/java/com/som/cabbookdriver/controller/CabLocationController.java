e53b60a42671e2f23a2ed8d824c6211670db13d4
Somnath Mukherjee <somnathmukherjee@Somnaths-MBP.bbrouter>
2024-05-18 14:29:09 +0530

    producer application is completer

--- cab-book-driver/src/main/java/com/som/cabbookdriver/controller/CabLocationController.java
+++ cab-book-driver/src/main/java/com/som/cabbookdriver/controller/CabLocationController.java
@@ -4,6 +4,7 @@ import com.som.cabbookdriver.service.CabLocationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,8 +15,14 @@ import java.util.Map;
 public class CabLocationController {
     @Autowired
     CabLocationService cabLocationService;
-
-    public ResponseEntity updatedLocation(){
+   @PutMapping
+    public ResponseEntity updatedLocation() throws InterruptedException {
+       int range = 100;
+       while(range > 0){
+           cabLocationService.booleanUpdateLocation(Math.random() + ", "+Math.random());
+           Thread.sleep(1000);
+           range--;
+       }
         return new ResponseEntity<>(Map.of("message", "Location updated"), HttpStatus.OK);
     }
 }
--- cab-book-driver/src/main/java/com/som/cabbookdriver/service/CabLocationService.java
+++ cab-book-driver/src/main/java/com/som/cabbookdriver/service/CabLocationService.java
@@ -1,4 +1,16 @@
 package com.som.cabbookdriver.service;
 
+import com.som.cabbookdriver.constant.AppConstant;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CabLocationService {
+    @Autowired private KafkaTemplate<String, Object> kafkaTemplate;
+
+    public boolean booleanUpdateLocation(String location){
+        kafkaTemplate.send(AppConstant.CAB_LOCATION, location);
+        return true;
+    }
 }
